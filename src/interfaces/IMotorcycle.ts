057739ac2ea2048cc1d675f25ec2361ccf45ea2b
Lalá Nametala <lalanametala@gmail.com>
2022-09-13 22:27:18 -0300

    :sparkles: feat(requisito 18): Crie a interface IMotorcycle a partir da Interface IVehicle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const MotorcycleZodSchema = VehicleZodSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number({
+    required_error: 'engineCapacity is required',
+    invalid_type_error: 'engineCapacity must be a number',
+  }).int()    
+    .positive()
+    .lte(2500),
+});
+
+type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
+
+export { MotorcycleZodSchema as CarZodSchema, IMotorcycle };
