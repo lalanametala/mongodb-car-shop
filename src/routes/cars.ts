bedccf95a96a2904b6bf163b1dfcfe9e779b2a7c
Lalá Nametala <lalanametala@gmail.com>
2022-09-13 22:11:11 -0300

    feat(requisito 13): Crie uma rota para o endpoint /cars/id, onde é possível atualizar o registro de um carro através do seu id

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -8,5 +8,6 @@ const carController = CreateCarController.make();
 route.post('/', (req, res) => carController.create(req, res));
 route.get('/:id', (req, res) => carController.readOne(req, res));
 route.get('/', (req, res) => carController.read(req, res));
+route.put('/:id', (req, res) => carController.update(req, res));
 
 export default route;
