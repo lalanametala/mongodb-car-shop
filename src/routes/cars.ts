b728f796edbb7879cc5459676580afc500cda870
Lalá Nametala <lalanametala@gmail.com>
2022-09-13 21:57:43 -0300

    :sparkles: feat(requisito 9): Crie uma rota para o endpoint /cars/id onde seja possível listar um único carro através do seu id

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -6,6 +6,7 @@ const route = Router();
 const carController = CreateCarController.make();
 
 route.post('/', (req, res) => carController.create(req, res));
+route.get('/:id', (req, res) => carController.readOne(req, res));
 route.get('/', (req, res) => carController.read(req, res));
 
 export default route;
